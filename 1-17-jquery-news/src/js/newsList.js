79c76e09bc03a22abb1878017ee5dd3c61ee3c1a
王朝靖 <826546753@qq.com>
2019-01-17 16:22:20 +0800

    test

---
+++ 1-17-jquery-news/src/js/newsList.js
@@ -0,0 +1,33 @@
+import 'bootcss';
+import '../css/newsList.stylus';
+import './newList/header'
+
+//  $.ajax({
+//      type: "get",
+//      url: "/api/php_correlation/news/news_list.php",
+//      data: null,
+//      dataType: "json",
+//      success: function(response) {
+//          console.log(response);
+//      }
+//  });
+
+//  $.ajax({
+//      type: "get",
+//      url: "/api/php_correlation/news/news_classify.php",
+//      data: null,
+//      dataType: "json",
+//      success: function(response) {
+//          console.log(response);
+//      }
+//  });
+
+//  $.ajax({
+//      type: "get",
+//      url: "/api/php_correlation/news/news_detail.php",
+//      data: null,
+//      dataType: "json",
+//      success: function(response) {
+//          console.log(response);
+//      }
+//  });
---
+++ 1-17-jquery-news/webpack.config.js
@@ -0,0 +1,107 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+const srcDir = path.resolve(__dirname, 'src');
+const nodeModPath = path.resolve(__dirname, 'node_modules');
+const extractPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        'A': path.resolve(__dirname, 'src/js/newsList.js'),
+        'B': path.resolve(__dirname, 'src/js/newsInfo.js'),
+        'vendor': ['bootstrap', 'jquery']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader'
+                }
+            ]
+        }, {
+            test: /\.stylus$/,
+            use: extractPlugin.extract({
+                fallback: {
+                    loader: 'style-loader'
+                },
+                use: [{
+                    loader: 'css-loader'
+                }, {
+                    loader: 'stylus-loader'
+                }]
+            })
+        }, {
+            test: /\.scss$/,
+            use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader'
+                },
+                {
+                    loader: 'sass-loader'
+                }
+            ]
+        }]
+    },
+    resolve: {
+        extensions: ['.js', ".css"],
+        modules: [srcDir, nodeModPath],
+        alias: {
+            jquery: path.resolve(__dirname, 'src/lib/jquery.js'),
+            bootstrap: path.resolve(__dirname, 'src/lib/bootstrap.js'),
+            bootcss: path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css')
+        }
+    },
+    plugins: [
+        new extractPlugin('css/[name].css'),
+        new CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/html/newsList.html'),
+            filename: 'newsList.html',
+            chunks: ['vendor', 'A']
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/html/newsInfo.html'),
+            filename: 'newsInfo.html',
+            chunks: ['vendor', 'B']
+        }),
+    ],
+    devServer: {
+        // contentBase: path.join(__dirname, "dist"),
+        contentBase: false,
+        port: "8080",
+        open: true,
+        hot: false,
+        openPage: 'newsList.html',
+        allowedHosts: [
+            "localhost:80"
+        ],
+        proxy: {
+            '/api': {
+                // target: 'http://localhost/1-11-user-admin/server',
+                target: 'http://localhost/server',
+                // target: 'http://api01.idataapi.cn:8000/news/qihoo',
+                pathRewrite: { '^/api': '' }
+            }
+        }
+    },
+}
